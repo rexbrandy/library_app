d1289e614dd441d3419c20ce7aa75418641c194a
Bailey Yates Armitage <yeliab@Baileys-MacBook-Pro.local>
2023-07-06 16:35:35 +0200

    Built index view and index.html for catalogs

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Book, BookInstance, Author, Genre
 
 def index(request):
-    return HttpResponse('Hello World')
+    num_books = Book.objects.all().count()
+    num_copies = BookInstance.objects.all().count()
+    num_avail = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_copies': num_copies,
+        'num_avail': num_avail,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
